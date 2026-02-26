2dd1412ea53c85e489a93264d592aad1f100f856
fancypantsonemillion <ravi.falsetti@gmail.com>
2026-02-26 17:40:47 -0500

    Simplify collision detection method

--- circleshape.py
+++ circleshape.py
@@ -22,5 +22,4 @@ class CircleShape(pygame.sprite.Sprite):
         pass
 
     def collides_with(self, other):
-        distance = self.position.distance_to(other.position)
-        return distance <= self.radius + other.radius
+        return self.position.distance_to(other.position) <= self.radius + other.radius
